e46a9963765baf230c024febe2609db34d4de9f4
Mitch VanDuyn <mitch@catprint.com>
2018-08-30 19:02:11 -0400

    added comments

--- hyper-operation/chat-app/app/hyperloop/stores/message_store.rb
+++ hyper-operation/chat-app/app/hyperloop/stores/message_store.rb
@@ -1,10 +1,20 @@
+# The MessageStore provides three class level methods all of which are reactive:
+# MessageStore.all - returns all messages 
+# MessageStore.user_name - return current user name 
+# MessageStore.online? - is the application connected
+
 class MessageStore < Hyperloop::Store
   state :messages, scope: :class, reader: :all
   state :user_name, scope: :class, reader: true
 
-  def self.online?
+  def self.online? 
+    # for purposes of our toy app we are online once messages gets set
     state.messages
   end
+
+  # The store listens for dispatches from our operations and updates
+  # state accordingly.  Notice that the Operation interface works the
+  # same regardless if the Operation is running remotely or locally.
 
   receives Operations::Join do |params|
     puts "receiving Operations::Join.run(#{params})"
